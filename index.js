193040615e42bfa2f02d29a9465d688d90bd3301
Jess Leyva <jessica.leyvat@yahoo.com>
2023-05-19 01:26:03 -0700

    added click event listeners to toggle buttons

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const featButton = document.getElementById("featPhotosBtn").addEventListener("click",btnAlert);
+const featButton = document.getElementById("featPhotosBtn").addEventListener("mouseover",btnAlert);
 
 function btnAlert(){
     alert(`You know you want these Airhead Extremes!`)
 }
-const lightBtn = document.getElementById("toggleLight")
+const lightBtn = document.getElementById("toggleLight").addEventListener("click",togglingModeLight);
 
-const darkBtn = document.getElementById("toggleDark")
+const darkBtn = document.getElementById("toggleDark").addEventListener("click",togglingModeDark)
+
+function togglingModeLight(){
+    document.body.style.backgroundColor = "white"
+     document.getElementById("motto").style.color = "black";
+    }
+function togglingModeDark(){
+    document.body.style.backgroundColor = "black"
+    document.getElementById("motto").style.color = "white";
+
+}
+
+
+
+
 
 
     

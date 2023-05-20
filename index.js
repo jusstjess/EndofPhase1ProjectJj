dcd7a673b339c6f73d7b027cb95d7efd7a84d7b7
Jess Leyva <jessica.leyvat@yahoo.com>
2023-05-19 22:37:22 -0700

    uses fetch to create a card for each candy, sets font color * image size.

--- index.js
+++ index.js
@@ -16,10 +16,55 @@ function togglingModeDark(){
     document.getElementById("motto").style.color = "white";
 
 }
+function fetchCandies() {
+     fetch("http://localhost:3000/candies").then(resp => resp.json()).then(candies => candies.forEach(candy => addingCandies(candy)))
+}
+
+fetchCandies()
+
+function addingCandies(candy) {
+    
+
+      let card = document.createElement('div');
+      card.classList.add("cardDiv");
+      //creating image element 
+      let image = document.createElement("img");
+      image.classList.add("imageContainer");
+      //creating h2 & h3 elements for candy name & price 
+      const h2 = document.createElement("h2");
+      const h3 = document.createElement("h3");
+      h2.setAttribute("id","candyName");
+      h3.setAttribute("id","candyPrice");
+   
+      //set image source
+      image.src = candy.image    
+      //set text content 
+      h2.textContent = candy.name;
+      h3.textContent = `Price: ${candy.price}`
+     
+
+      //appending h2, img cardContainer
+      card.appendChild(image);
+      card.appendChild(h2);
+      card.appendChild(h3);
+
+
+      document.getElementById("card-container").appendChild(card);
+}
 
 
 
 
+/*const candyCards = document.getElementById("candy-cards");
+    const cardContainer =   document.getElementsByClassName("card-container")
 
+   
+      //const cnewCandyCard = document.createElement("card");//
+      let img = document.createElement("img");
+      img.src = candy.image
+      let h2 = document.createElement("h2");
+      h2.textContent = candy.name;
+      cardContainer.appendChild(h2,candyCardNew,image)
+      candyCards.appendChild(cardContainer)*/
 
     

c53931b65e96161bbe3d7ba8f639d6997623fa22
Jess Leyva <jessica.leyvat@yahoo.com>
2023-05-21 00:17:46 -0700

    edits colors for paragraph and candy name in light mode

--- index.js
+++ index.js
@@ -7,17 +7,21 @@ const lightBtn = document.getElementById("toggleLight").addEventListener("click"
 
 const darkBtn = document.getElementById("toggleDark").addEventListener("click",togglingModeDark)
 
+
+
 function togglingModeLight(){
     document.body.style.backgroundColor = "white"
      document.getElementById("motto").style.color = "black";
-    }
+     document.getElementById("votingParagragh").style.color = "black";   }
 function togglingModeDark(){
     document.body.style.backgroundColor = "black"
     document.getElementById("motto").style.color = "white";
 
 }
+
+const fetchUrl = "http://localhost:3000/candies"
 function fetchCandies() {
-     fetch("http://localhost:3000/candies").then(resp => resp.json()).then(candies => candies.forEach(candy => addingCandies(candy)))
+     fetch(fetchUrl).then(resp => resp.json()).then(candies => candies.forEach(candy => addingCandies(candy)))
 }
 
 fetchCandies()
@@ -38,37 +42,46 @@ function addingCandies(candy) {
       let btn = document.createElement('button')
       btn.setAttribute('id', "votingBtn")
       btn.classList.add("button")
-      btn.textContent = `${candy.votes} votes received,
-      click here to VOTE`
+      
       //set image source
       image.src = candy.image    
       //set text content 
       h2.textContent = candy.name+": "+ candy.price;
      
-
-      //appending h2, img 
-  
+     //adds button event listener CLICK  to update votes 
+      btn.addEventListener("click",updateVotes);
+      
+      function updateVotes(){
+             let currentVotes = (candy.votes +=1)
+             btn.textContent = `${currentVotes} votes received,
+      click here to VOTE`
+      const data = {"votes": `${currentVotes}` }
+      fetch(`http://localhost:3000/candies/${candy.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then(response => response.json())
+          // Handle the response)
+        .catch((error) => {
+          // Handle any errors
+        });
+          }
+          btn.textContent = `${candy.votes} votes received,
+      click here to VOTE`
+      
+      //appending h2, btn 
       card.appendChild(h2);
       card.appendChild(btn);
 
+      //appending image , card 
       document.getElementById("card-container").appendChild(image);
       document.getElementById("card-container").appendChild(card);
+      } 
 
-}
+      
 
 
-
-
-/*const candyCards = document.getElementById("candy-cards");
-    const cardContainer =   document.getElementsByClassName("card-container")
-
-   
-      //const cnewCandyCard = document.createElement("card");//
-      let img = document.createElement("img");
-      img.src = candy.image
-      let h2 = document.createElement("h2");
-      h2.textContent = candy.name;
-      cardContainer.appendChild(h2,candyCardNew,image)
-      candyCards.appendChild(cardContainer)*/
 
     
